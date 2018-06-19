f16a575820a26bd82ce7771e63b07eecd0abd743
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-06-19 16:46:49 +0530

    assigned result of '$this->user_model->get_users()' to an array named '$data['results']', then passed this array to 'view()' method as a second argument

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -5,15 +5,17 @@ class Users extends CI_Controller
     public function show()
     {
         // $this->load->model('user_model');
-        $result = $this->user_model->get_users();
+        $data['results'] = $this->user_model->get_users();
+
+        $this->load->view('user_view', $data);
 
         // Processing the result
-        foreach($result as $object)
-        {
-            echo $object->id . "<br/>";
-            echo $object->username . "<br/>";
-            echo $object->password . "<br/>";
-        }
+        // foreach($result as $object)
+        // {
+        //     echo $object->id . "<br/>";
+        //     echo $object->username . "<br/>";
+        //     echo $object->password . "<br/>";
+        // }
         
     }
 }
