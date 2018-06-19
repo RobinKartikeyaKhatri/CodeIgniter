901ccd075c54dfd2d85bcc57ad0a907ab8469205
robinkartikeyakhatri <kartikrobin@gmail.com>
2018-06-19 15:07:33 +0530

    processed '$result' variable via foreach loop to get result

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -6,6 +6,14 @@ class Users extends CI_Controller
     {
         // $this->load->model('user_model');
         $result = $this->user_model->get_users();
+
+        // Processing the result
+        foreach($result as $object)
+        {
+            echo $object->id . "<br/>";
+            echo $object->username . "<br/>";
+            echo $object->password . "<br/>";
+        }
         
     }
 }
